c115f6dfb4c1b3dbe397c43fa8c7f0f87ac1a7ad
idwangmo <idwangmo@protonmail.com>
2020-09-25 22:30:37 +0800

    fix(redis): redis configuration

---
+++ whitebird-commons/whitebird-redis-spring-boot-starer/src/main/java/top/idwangmo/whitebird/redisspringbootstarer/config/WhitebirdCacheConfiguration.java
@@ -0,0 +1,33 @@
+package top.idwangmo.whitebird.redisspringbootstarer.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 用于存储基本的配置类.
+ *
+ * @author idwangmo
+ */
+@EnableCaching
+public class WhitebirdCacheConfiguration extends CachingConfigurerSupport {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer())).disableCachingNullValues());
+    }
+
+}
