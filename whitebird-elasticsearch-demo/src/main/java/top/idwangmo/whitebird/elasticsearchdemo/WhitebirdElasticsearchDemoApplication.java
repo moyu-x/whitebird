e80842a5aa9650e91c5114ee87aeba0a4e47d9a2
idwangmo <idwangmo@protonmail.com>
2020-10-08 22:56:27 +0800

    feat: 🎸 elasticsearch
    spring data elasticsearch demo services

---
+++ whitebird-elasticsearch-demo/src/main/java/top/idwangmo/whitebird/elasticsearchdemo/WhitebirdElasticsearchDemoApplication.java
@@ -0,0 +1,14 @@
+package top.idwangmo.whitebird.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WhitebirdElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitebirdElasticsearchDemoApplication.class, args);
+	}
+
+}
---
+++ whitebird-elasticsearch-demo/src/test/java/top/idwangmo/whitebird/elasticsearchdemo/WhitebirdElasticsearchDemoApplicationTests.java
@@ -0,0 +1,168 @@
+package top.idwangmo.whitebird.elasticsearchdemo;
+
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import top.idwangmo.whitebird.elasticsearchdemo.config.ElasticsearchConstant;
+import top.idwangmo.whitebird.elasticsearchdemo.entity.UserEsEntity;
+import top.idwangmo.whitebird.elasticsearchdemo.repository.UserEsRepository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+
+@SpringBootTest
+class WhitebirdElasticsearchDemoApplicationTests {
+
+	@Autowired
+	private RestHighLevelClient highLevelClient;
+
+	@Autowired
+	private ElasticsearchOperations elasticsearchOperations;
+
+	@Autowired
+	private UserEsRepository userEsRepository;
+
+	@Autowired
+	private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+	@Test
+	void testESClient() throws IOException {
+		GetRequest getRequest = new GetRequest(ElasticsearchConstant.USER_INDEX, "3333");
+		GetResponse getResponse = highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+		System.out.println(getResponse.getIndex());
+		System.out.println(getResponse.toString());
+	}
+
+	@Test
+	public void testSave() {
+		UserEsEntity user = new UserEsEntity();
+		user.setLastName("测试1");
+		user.setUserType("测试");
+
+		IndexCoordinates indexCoordinates = elasticsearchOperations.getIndexCoordinatesFor(user.getClass());
+
+		IndexQuery indexQuery = new IndexQueryBuilder().withId(user.getId()).withObject(user).build();
+
+		String documentId = elasticsearchOperations.index(indexQuery, indexCoordinates);
+
+		System.out.println(documentId);
+	}
+
+	@Test
+	public void testElasticsearchOperations() {
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+				.withQuery(new MatchAllQueryBuilder())
+				.build();
+
+		System.out.println(searchQuery.getQuery().toString());
+
+		SearchHits<UserEsEntity> searchHits = elasticsearchOperations.search(searchQuery, UserEsEntity.class);
+		long count = searchHits.getTotalHits();
+		System.out.println(count);
+
+		List<SearchHit<UserEsEntity>> list = searchHits.getSearchHits();
+		list.forEach(System.out::println);
+	}
+
+	@Test
+	void testSave2() {
+		UserEsEntity entity = new UserEsEntity();
+		entity.setId(UUID.randomUUID().toString());
+		entity.setLastName("测试2");
+		entity.setUserType("还是测试");
+		userEsRepository.save(entity);
+	}
+
+	@Test
+	void testNormalQuery() {
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+
+		MatchQueryBuilder matchQuery = QueryBuilders.matchQuery(ElasticsearchConstant.LAST_NAME_FIELD, "测试");
+		queryBuilder.must(matchQuery);
+
+		System.out.println(queryBuilder.toString());
+
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+				.withQuery(queryBuilder)
+				.build();
+
+		SearchHits<UserEsEntity> search = elasticsearchOperations.search(searchQuery, UserEsEntity.class);
+		search.forEach(hits -> {
+			UserEsEntity content = hits.getContent();
+			System.out.println(content.toString());
+		});
+	}
+
+	@Test
+	void queryMatchList() {
+		List<UserEsEntity> result = userEsRepository.findByLastNameContains("测试");
+		result.forEach(System.out::println);
+	}
+
+	@Test
+	void testElasticsearchRestTemplate() {
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+				.must(QueryBuilders.matchQuery(ElasticsearchConstant.LAST_NAME_FIELD, "测试"));
+
+		System.out.println(queryBuilder.toString());
+
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+				.withQuery(queryBuilder)
+				.build();
+
+		SearchHits<UserEsEntity> search = elasticsearchRestTemplate.search(searchQuery, UserEsEntity.class);
+		search.forEach(System.out::println);
+	}
+
+	@Test
+	void deleteTest() {
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+				.must(QueryBuilders.matchQuery(ElasticsearchConstant.LAST_NAME_FIELD, "2"));
+
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+				.withQuery(queryBuilder)
+				.build();
+
+		elasticsearchRestTemplate.delete(searchQuery, UserEsEntity.class, IndexCoordinates.of(ElasticsearchConstant.USER_INDEX));
+	}
+
+	@Test
+	void testAggregations() {
+
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+		MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery(ElasticsearchConstant.LAST_NAME_FIELD, "2");
+		RangeQueryBuilder ageRange = QueryBuilders.rangeQuery(ElasticsearchConstant.AGE).from(1).to(20);
+
+		queryBuilder.must(matchQueryBuilder).must(ageRange);
+
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+				.withQuery(queryBuilder)
+				.withSearchType(SearchType.DEFAULT)
+				.addAggregation(AggregationBuilders.terms("subjects").field(ElasticsearchConstant.LAST_NAME_FIELD))
+				.build();
+
+		System.out.println(searchQuery.getQuery().toString());
+		SearchHits<UserEsEntity> search = elasticsearchRestTemplate.search(searchQuery, UserEsEntity.class, IndexCoordinates.of("user_index"));
+
+		search.forEach(System.out::println);
+
+	}
+}
